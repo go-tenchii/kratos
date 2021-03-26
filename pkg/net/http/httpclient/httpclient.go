7b1c6a379cffa2eed4b393462c0ba11cf5d5ab21
unknown <wangzhuoran@tenchii.com>
2021-03-26 17:22:14 +0800

    update

--- pkg/net/http/httpclient/httpclient.go
+++ pkg/net/http/httpclient/httpclient.go
@@ -2,7 +2,6 @@ package httpclient
 
 import (
 	"context"
-	"github.com/go-tenchii/kratos/pkg/net/metadata"
 	"github.com/go-tenchii/kratos/pkg/net/trace"
 	"net/http"
 	"strconv"
@@ -11,25 +10,30 @@ import (
 const _defaultComponentName = "net/http"
 
 func Do(ctx context.Context, dc *http.Client, req *http.Request) (*http.Response, error) {
+	var (
+		t  trace.Trace
+		ok bool
+	)
+
+	if t, ok = trace.FromContext(ctx); ok {
+		req.Header.Set(trace.KratosTraceID, t.TraceID())
+	}
 	t, err := trace.Extract(trace.HTTPFormat, req.Header)
 	if err != nil {
 		var opts []trace.Option
 		if ok, _ := strconv.ParseBool(trace.KratosTraceDebug); ok {
 			opts = append(opts, trace.EnableDebug())
 		}
-		t = trace.New("external/"+req.URL.Path, opts...)
+		t = trace.New(req.URL.Path, opts...)
 	}
-	t.SetTitle(req.URL.Path)
+
+	t.SetTitle("external/" + req.URL.Path)
 	t.SetTag(trace.String(trace.TagComponent, _defaultComponentName))
 	t.SetTag(trace.String(trace.TagHTTPMethod, req.Method))
 	t.SetTag(trace.String(trace.TagHTTPURL, req.URL.String()))
-	t.SetTag(trace.String(trace.TagSpanKind, "server"))
-	// business tag
-	t.SetTag(trace.String("caller", metadata.String(ctx, metadata.Caller)))
-	// export trace id to user.
 	ctx = trace.NewContext(ctx, t)
 	resp, err := dc.Do(req)
-	t.Finish(&err)
+	defer t.Finish(&err)
 	if err != nil {
 		return nil, err
 	}
